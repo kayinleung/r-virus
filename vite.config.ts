b87cb9249a1c2c04a7f090933d3f57a5129485dd
John Fisher <jgf5013@gmail.com>
2025-06-16 19:57:30 +0200

    fix(cicd): setting up PR env to use relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,6 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-const getBaseUrl = () => {
-  // For main branch deploys and local development use the basic GH Pages projct path
-  if (process.env.GITHUB_EVENT_NAME !== 'pull_request') {
-    return `/r-virus/`;
-  }
-
-  const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
-  const prNumber = process.env.GITHUB_HEAD_REF?.split('/')[2] || ''; // Extract PR number from branch name (e.g., 'pull/3/head')
-  const eventNumber = process.env.GITHUB_EVENT_NUMBER || prNumber;
-
-  if (repoName && eventNumber) {
-    // For PRs on GitHub Pages, the base is /{repo_name}/pr-previews/{pr_number}/
-    return `/${repoName}/pr-previews/${eventNumber}/`;
-  }
-};
-
 // https://vite.dev/config/
 export default defineConfig({
   build: {
@@ -53,5 +37,5 @@ export default defineConfig({
       },
     },
   },
-  base: getBaseUrl(),
+  base: process.env.BASE_URL ?? '/r-virus/',
 })
