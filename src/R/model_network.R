3e77ad20f3dc12c4681d1937b5c9535925f1f7c2
KaYin Leung <mail.kyleung@gmail.com>
2025-06-19 20:53:40 +0200

    fix: calculate parameter value from input

--- src/R/model_network.R
+++ src/R/model_network.R
@@ -2,9 +2,14 @@ library(PBSddesolve)
 library(escape2024)
 
 tryCatch({
+  degree = list(degree_distribution = "poisson", lambda = 3)
+  var = var_degree(degree)
+  avg = mean_degree(degree)
+  c_degree = (var + avg^2 - avg) / avg
+
   infectiousness_rate = 2 / `${serial_interval}`
   recovery_rate = 2 / `${serial_interval}`
-  transmission_rate = `${reproduction_number}` / `${serial_interval}`
+  transmission_rate = `${reproduction_number}` * recovery_rate / c_degree
 
   `${model_type}`(
     transmission_rate = transmission_rate,
