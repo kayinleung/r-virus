09fd6b927fc653a9133a2f54211fa23afbbe5a0b
kayinleung <kayinleung@users.noreply.github.com>
2025-06-19 18:54:18 +0000

    deploy: 3e77ad20f3dc12c4681d1937b5c9535925f1f7c2

---
+++ assets/model_network-mYMtqdfR.js
@@ -0,0 +1,29 @@
+const e=`library(PBSddesolve)
+library(escape2024)
+
+tryCatch({
+  degree = list(degree_distribution = "poisson", lambda = 3)
+  var = var_degree(degree)
+  avg = mean_degree(degree)
+  c_degree = (var + avg^2 - avg) / avg
+
+  infectiousness_rate = 2 / \`\${serial_interval}\`
+  recovery_rate = 2 / \`\${serial_interval}\`
+  transmission_rate = \`\${reproduction_number}\` * recovery_rate / c_degree
+
+  \`\${model_type}\`(
+    transmission_rate = transmission_rate,
+    infectiousness_rate = infectiousness_rate,
+    recovery_rate = recovery_rate,
+    time_end = \`\${time_end}\`,
+    increment = \`\${increment}\`,
+    population_size = \`\${population_size}\`,
+    seed_infected = \`\${seed_infected}\`,
+    degree_distribution = "poisson",
+    infection = "SEIR",
+    lambda = 1
+  )
+  flush.console()
+}, finally = {
+  flush.console()
+})`;export{e as default};
