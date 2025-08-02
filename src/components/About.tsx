fb275070e9dd57fa09dc56168c887ed39f997bc0
John Fisher <jgf5013@gmail.com>
2025-08-02 19:16:54 +0200

    chore: pictures in About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,17 +8,19 @@ export const About = () => {
       <p>This application is designed to simulate the spread of a virus through a population.</p>
       <p>It uses WebR to run R code in the browser, allowing for interactive data analysis and visualization.</p>
       <p>For more information, visit the project's GitHub repository.</p>
-      <p>Data modeling done by Ka Yin Leung; Web application development by John Fisher.</p>
+      <p>Data science development and data modeling done by <a href="https://www.linkedin.com/in/kayinleung/">Ka Yin Leung</a>; Web application development by <a href="https://www.linkedin.com/in/jgf5013/">John Fisher</a>.</p>
       <div className={styles.aboutImages}>
         <Image
           src="./Kayin.webp"
           className={styles.aboutImage}
           alt="Ka Yin Leung"
+          onClick={() => window.open("https://www.linkedin.com/in/kayinleung/", "_blank")}
         />
         <Image
           src="./John.webp"
           className={styles.aboutImage}
           alt="John Fisher"
+          onClick={() => window.open("https://www.linkedin.com/in/jgf5013/", "_blank")}
         />
       </div>
     </Paper>
--- src/components/InputControls.tsx
+++ src/components/InputControls.tsx
@@ -25,48 +25,51 @@ const InputControls = () => {
 
   return (
     <Paper p="sm" className={styles.inputControls}>
-      <Refresh />
-      <Select
-        label="Archetype"
-        value={currentForm.value.archetype}
-        onChange={handleArchetypeChange}
-        data={archetypeOptions}
-      />
-      <NumberInput
-        label="Reproduction Number"
-        value={currentForm.value.reproductionNumber}
-        min={archetypeCorollaries.value.reproductionNumber.range[MIN_INDEX]}
-        max={archetypeCorollaries.value.reproductionNumber.range[MAX_INDEX]}
-        onChange={(value) => handleNumberChange({ field: "reproductionNumber", value: Number(value) }) }
-      />
-      <NumberInput
-        label="Serial Interval"
-        value={currentForm.value.serialInterval}
-        min={archetypeCorollaries.value.serialInterval.range[MIN_INDEX]}
-        max={archetypeCorollaries.value.serialInterval.range[MAX_INDEX]}
-        onChange={(value) => handleNumberChange({ field: "serialInterval", value: Number(value) }) }
-      />
-      <NumberInput
-        label="Mean Degree"
-        value={currentForm.value.mu}
-        min={0}
-        max={100}
-        onChange={(value) => handleNumberChange({ field: "mu", value: Number(value) }) }
-      />
-      <NumberInput
-        label="Dispersion"
-        value={currentForm.value.dispersion}
-        min={0}
-        max={10}
-        onChange={(value) => handleNumberChange({ field: "dispersion", value: Number(value) }) }
-      />
-      <NumberInput
-        label="Population"
-        value={currentForm.value.populationSize}
-        min={10}
-        max={1e10}
-        onChange={(value) => handleNumberChange({ field: "populationSize", value: Number(value) }) }
-      />
+
+      <Refresh className={styles.refresh} />
+      <div>
+        <Select
+          label="Archetype"
+          value={currentForm.value.archetype}
+          onChange={handleArchetypeChange}
+          data={archetypeOptions}
+        />
+        <NumberInput
+          label="Reproduction Number"
+          value={currentForm.value.reproductionNumber}
+          min={archetypeCorollaries.value.reproductionNumber.range[MIN_INDEX]}
+          max={archetypeCorollaries.value.reproductionNumber.range[MAX_INDEX]}
+          onChange={(value) => handleNumberChange({ field: "reproductionNumber", value: Number(value) }) }
+        />
+        <NumberInput
+          label="Serial Interval"
+          value={currentForm.value.serialInterval}
+          min={archetypeCorollaries.value.serialInterval.range[MIN_INDEX]}
+          max={archetypeCorollaries.value.serialInterval.range[MAX_INDEX]}
+          onChange={(value) => handleNumberChange({ field: "serialInterval", value: Number(value) }) }
+        />
+        <NumberInput
+          label="Mean Degree"
+          value={currentForm.value.mu}
+          min={0}
+          max={100}
+          onChange={(value) => handleNumberChange({ field: "mu", value: Number(value) }) }
+        />
+        <NumberInput
+          label="Dispersion"
+          value={currentForm.value.dispersion}
+          min={0}
+          max={10}
+          onChange={(value) => handleNumberChange({ field: "dispersion", value: Number(value) }) }
+        />
+        <NumberInput
+          label="Population"
+          value={currentForm.value.populationSize}
+          min={10}
+          max={1e10}
+          onChange={(value) => handleNumberChange({ field: "populationSize", value: Number(value) }) }
+        />
+      </div>
     </Paper>
   )
 };
