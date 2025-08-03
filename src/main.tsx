9962f1afe4180951f6f68e2ad65b8997031b5b51
John Fisher <jgf5013@gmail.com>
2025-08-03 10:11:22 +0200

    fix: base url with react router

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,17 @@ import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
+// console.log('main - process.env.BASE_URL =', process.env.BASE_URL);
 // TODO: Add another "Info" page
 const router = createBrowserRouter([
   {
-    path: "/r-virus/",
+    path: "/",
     element: <App />,
     errorElement: <div>Error</div>,
   },
-]);
+], {
+  basename: window.location.pathname,
+});
 
 createRoot(document.getElementById('root')!).render(
   <>
