f976874cc1346e708ebd7ea99df5046e3097c4bd
John Fisher <jgf5013@gmail.com>
2025-08-04 21:03:24 +0200

    fix: light mode styles

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import styles from './About.module.css';
 
 export const About = () => {
   return (
-    <Paper className={styles.about} p="sm">
+    <Paper shadow="xs" className={styles.about} p="sm">
       <p>This application is designed to simulate the spread of a virus through a population. It uses WebR to run R code in the browser, allowing for interactive data analysis and visualization.</p>
       <p>Data science development and data modeling done by <a href="https://www.linkedin.com/in/kayinleung/">Ka Yin Leung</a>; Web application development by <a href="https://www.linkedin.com/in/jgf5013/">John Fisher</a>.</p>
       <div className={styles.aboutImages}>
