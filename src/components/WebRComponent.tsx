b7419a814160a7ced610143a490513b21f43ac21
KaYin Leung <mail.kyleung@gmail.com>
2025-09-17 11:59:04 +0000

    task: remove variable

--- src/components/WebRComponent.tsx
+++ src/components/WebRComponent.tsx
@@ -52,7 +52,6 @@ export const WebRComponent = () => {
         .replace(/`\${simulation_id}`/g, simulationId)
         .replace(/`\${population_size}`/g, String(currentForm.value.populationSize))
         .replace(/`\${serial_interval}`/g, String(currentForm.value.serialInterval))
-        .replace(/`\${time_end}`/g, String(currentForm.value.timeEnd))
         .replace(/`\${model_type}`/g, currentForm.value.modelType)
         .replace(/`\${reproduction_number}`/g, String(currentForm.value.reproductionNumber))
         .replace(/`\${increment}`/g, String(currentForm.value.increment))
