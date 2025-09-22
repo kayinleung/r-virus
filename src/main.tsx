8539bbd5c05fa2dffba69acaf0f064aac8412ba6
John Fisher <jgf5013@gmail.com>
2025-09-22 20:01:32 +0200

    fix: unneeded d.ts file for webr

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-
-// console.log('main - process.env.BASE_URL =', process.env.BASE_URL);
-// TODO: Add another "Info" page
 const router = createBrowserRouter([
   {
     path: "/",
